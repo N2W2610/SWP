ffc2a045771ea5a222d150f5e8a4948246b33eb1
hoangtvse170014 <hoangtvse170014@fpt.edu.vn>
2025-06-13 16:05:15 +0700

    Add files via upload

---
+++ src/java/model/UserFeedback.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Timestamp;
+
+public class UserFeedback {
+    private int feedbackId;
+    private int userId;
+    private String content;
+    private Timestamp feedbackDate;
+    private String status;
+
+    public int getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(int feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getFeedbackDate() {
+        return feedbackDate;
+    }
+
+    public void setFeedbackDate(Timestamp feedbackDate) {
+        this.feedbackDate = feedbackDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
